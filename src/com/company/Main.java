1a31484418a7fdcafd1abb14894410597ba6657d
muna pokharel <pokharel.muna1@gmail.com>
2020-09-09 14:19:56 -0500

    First attempt committing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("hello world ! from github.com..");
-        System.out.print("Sandes Lamsal");
-        //System.out.println("Java Automation\n");
-
-        /*
-        //4/6/2020 2:20 pm
-        int[] intArray={100,200,300};
-        for (int a:intArray){
-            System.out.println(a);
-        }
-        */
-        //https://github.com/qalamsal/gitTest_HelloWorld_Project.git
+        System.out.print("Muna Pokharel");
     }
 }
