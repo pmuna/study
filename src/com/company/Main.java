c6ab98b3c037ed0bcb3d728293a0ae9fc236b1fe
muna pokharel <pokharel.muna1@gmail.com>
2020-09-10 14:23:42 -0500

    switch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("hello world ! from github.com..");
-        System.out.print("Muna Pokharel");
+        System.out.println("hello world ! ");
+        System.out.println("Muna Pokharel");
         System.out.println("Welcome to IT Sutra");
+        System.out.println(("This is branch 2"));
+
     }
 }
